5cb1b4fbc330b50b401097b4d53c690520961d40
JoWilks25 <joannewilkins88@gmail.com>
2023-12-07 12:41:18 +0000

    Get Sorting working

--- components/ListHeaderComponent.tsx
+++ components/ListHeaderComponent.tsx
@@ -4,21 +4,26 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { styles } from '../styles/styles';
+import { SortByType } from '../App';
 
+type ListHeaderComponentProps = {
+  sortBy: SortByType;
+  setSortBy: React.Dispatch<React.SetStateAction<SortByType>>;
+};
 
-const ListHeaderComponent = () => {
+const ListHeaderComponent = ({sortBy, setSortBy}: ListHeaderComponentProps) => {
   return (
     <View style={styles.headerView}>
       <Icon.Button
-        name={true ? 'sort-amount-asc' : 'sort-amount-desc'}
+        name={sortBy.sortName === 'expiryDate' && sortBy.sortOrder === 'asc' ? 'sort-amount-asc' : 'sort-amount-desc'}
         // backgroundColor="#3b5998"
-        onPress={() => console.log('sort by date')}
+        onPress={() => setSortBy({ sortName: 'expiryDate', sortOrder: sortBy.sortOrder === 'asc' ? 'desc' : 'asc' })}
       >
        Expiry Date
       </Icon.Button>
       <Icon.Button
-        name={true ? 'sort-alpha-asc' : 'sort-alpha-desc'}
-        onPress={() => console.log('sort by alphabetical')}
+        name={sortBy.sortName === 'name' && sortBy.sortOrder === 'asc' ? 'sort-alpha-asc' : 'sort-alpha-desc'}
+        onPress={() => setSortBy({ sortName: 'name', sortOrder: sortBy.sortOrder === 'asc' ? 'desc' : 'asc' })}
       >
         Alphabetical
       </Icon.Button>
