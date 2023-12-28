2470f01d258822c6468a3b2fe3aa67892ed8094e
JoWilks25 <joannewilkins88@gmail.com>
2023-12-28 13:23:33 +0000

    Get App working on iphone

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import MainScreen from './screens/MainScreen';
+// import {Notifications} from 'react-native-notifications';
 
 
 const App = () => {
+  // let localNotification: any = Notifications.postLocalNotification({
+  //   body: "Local notification!",
+  //   title: "Local Notification Title",
+  //   sound: "chime.aiff",
+  //   // silent: false,
+  //   category: "SOME_CATEGORY",
+  //   userInfo: { },
+  //   fireDate: new Date(),
+  // });
+  // console.log('localNotification', localNotification)
+
   return (<MainScreen />)
 };
 
