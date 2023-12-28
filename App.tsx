063b3179f41de5535bd5e92dc48f3619d0ada069
JoWilks25 <joannewilkins88@gmail.com>
2023-12-28 13:58:14 +0000

    Get base notifications functions working

--- App.tsx
+++ App.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import MainScreen from './screens/MainScreen';
-// import {Notifications} from 'react-native-notifications';
+// import NotificationComponent from './screens/Notifications';
+import * as Notifications from 'expo-notifications';
+
+// First, set the handler that will cause the notification
+// to show the alert
+
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldShowAlert: true,
+//     shouldPlaySound: false,
+//     shouldSetBadge: false,
+//   }),
+// });
+
+// // Second, call the method
+
+// Notifications.scheduleNotificationAsync({
+//   content: {
+//     title: 'Test Notification',
+//     body: "Expiring in 1 day",
+//   },
+//   trigger: null,
+// });
+
+const schedulePushNotification = async () => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: {
+      channelId: 'test',
+      repeats: false,
+      seconds: 10,
+    },
+  });
+}
 
 
 const App = () => {
-  // let localNotification: any = Notifications.postLocalNotification({
-  //   body: "Local notification!",
-  //   title: "Local Notification Title",
-  //   sound: "chime.aiff",
-  //   // silent: false,
-  //   category: "SOME_CATEGORY",
-  //   userInfo: { },
-  //   fireDate: new Date(),
-  // });
-  // console.log('localNotification', localNotification)
-
-  return (<MainScreen />)
+  return (
+    <Fragment>
+      <MainScreen />
+      {/* <NotificationComponent /> */}
+    </Fragment>
+  )
 };
 
 export default App;
