e2d09ff14044082ec1cf6dabc4fe7480ed68dd14
JoWilks25 <joannewilkins88@gmail.com>
2024-01-11 12:48:18 +0000

    Before trying EAS

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react';
 import MainScreen from './screens/MainScreen';
-// import NotificationComponent from './screens/Notifications';
 
 const App = () => {
   return (
     <Fragment>
       <MainScreen />
-      {/* <NotificationComponent /> */}
     </Fragment>
   )
 };
